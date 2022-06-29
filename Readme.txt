20183a7664abc43f277a7126576420d90da1ca09
Yunpeng Li <yli4@umbc.edu>
2022-06-29 13:09:12 -0400

    Try out Hal explorer and add Spring Actuator

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,10 @@
 http://localhost:7777/SpringBoot_Thymeleaf_Demo/
+
+Access Hal Explorer:
+http://localhost:7777/SpringBoot_Thymeleaf_Demo/webjars/hal-explorer/1.1.0/index.html
+
+Hal Explorer:
+https://toedter.github.io/hal-explorer/snapshot/reference-doc/
+
+Access Actuator:
+http://localhost:7777/SpringBoot_Thymeleaf_Demo/actuator
--- src/main/java/org/dragon/yunpeng/thymeleaf/repositories/UserRepository.java
+++ src/main/java/org/dragon/yunpeng/thymeleaf/repositories/UserRepository.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.dragon.yunpeng.thymeleaf.entities.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
+	//@RestResource(rel = "findByName", path="findByName")
 	List<User> findByName(String name);
 
 }
